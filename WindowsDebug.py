c4c32cd097052b4e492319c99d81a7cecfa891cf
Tys vanZeyl <ttis3.14@gmail.com>
2020-03-13 20:55:19 -0700

    delete WindowsDebug.py SIM868 declaration and import from SIM868.py

--- WindowsDebug.py
+++ WindowsDebug.py
@@ -1,29 +1,10 @@
 import math
 import serial
 import time
-
-class SIM868:
-    def __init__(self):
-        self.sp = serial.Serial('COM5', 115200)
-
-    def get(self):
-        return_string = ""
-        while self.sp.in_waiting > 0:
-            text = str(self.sp.readline().decode('UTF-8').strip())
-            print('SIM868: ' + text)
-            return_string += text
-        return return_string
-
-    def write(self, command):
-        command = str(command)
-        print("You   : " + command)
-        self.sp.write(command.encode())
-        self.sp.write("\n".encode())
-        self.get()
+import SIM868
 
 
-
-SIM = SIM868()
+SIM = SIM868.SIM868()
 SIM.write("AT+CGNSPWR=1")
 SIM.write("AT+CGNSURC=1")
 
