52bbc4222d403300923560f132bbbd775e4a25fa
Tys vanZeyl <ttis3.14@gmail.com>
2020-03-13 22:36:33 -0700

    debugging rpi serial communication

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ import math
 import time
 import SIM868
 
+SIM = SIM868.SIM868()
+SIM.write("AT+CGNSPWR=1")
+SIM.write("AT+CGNSURC=1")
+
+n_cont = "y"
+while n_cont != "n":
+    SIM.get()
+    n_cont = input("continue?")
